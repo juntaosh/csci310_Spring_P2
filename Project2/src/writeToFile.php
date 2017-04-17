6f256a58f2e79102d65a74c38a9ac27b7f0529f1
Lifan <lemonloic@gmail.com>
2017-04-16 19:07:36 -0700

    search author fixes

--- Project2/src/writeToFile.php
+++ Project2/src/writeToFile.php
@@ -1,5 +1,5 @@
 <?php
-	$file_handle = fopen("../tmp/progress.md", "r");// or die ('die');
-	//fwrite($file_handle,"0");
+	$file_handle = fopen("../tmp/progress.md", "w") or die ('die');
+	fwrite($file_handle,'0');
 	fclose($file_handle);
 ?>
